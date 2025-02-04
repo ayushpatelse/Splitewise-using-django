0f90231ec92a7eea90c1e5e007e240ee376da698
Ayush <ayushpatelse@gmail.com>
2025-02-04 08:21:46 -0500

    Added delete_ask function

--- mysite/static/javascripts/main.js
+++ mysite/static/javascripts/main.js
@@ -3,24 +3,4 @@ function delete_ask(){
     name.classList.remove('hidden')
     console.log('functino called')
 }
-const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-type: 'bar',
-data: {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
-  }]
-},
-options: {
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  }
-}
-});
 
