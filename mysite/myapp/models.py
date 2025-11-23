5d1e3965161652db01f07b002c0725f878abd993
ayushpatelse <ayushpatelse@gmail.com>
2025-11-23 16:41:07 -0500

    Changed amount value from Float to Decimal Added clean functions to make model robust

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Person(models.Model):    
@@ -21,7 +22,11 @@ class Group(models.Model):
     
 class Expense(models.Model):
     expenseName = models.CharField(max_length=100,default='item')
-    amount = models.FloatField(default=0.0)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
     description = models.TextField(default=None,blank=True)
     payer = models.ForeignKey(Person,on_delete=models.CASCADE)
     group = models.ForeignKey(Group,on_delete=models.CASCADE)
@@ -31,25 +36,71 @@ class Expense(models.Model):
     def __str__(self):
         return self.expenseName
     
+    def clean(self):
+        error = {}
+
+        if self.amount <=0:
+            error['amount'] = f'Expense Value Cannot be Negative or Zero'
+        
+        if self.payer not in self.group.members.all():
+            error['payer'] = f'The {self.payer.name} is not in {self.group.groupName}'
+
+        if error:
+            raise ValidationError(error)
+
+    def save(self,*args,**kwargs):
+        self.full_clean()
+        return super().save()
+
 class ExpenseShare(models.Model):
-    SPLIT_TYPE = {
-        (1,'equal'),
-        (2,'unequal'),
-        (3,'percent'),
-    }
-    expsAmount = models.FloatField()
+    SPLIT_TYPE = [
+    ('equal', 'Equal'),
+    ('unequal', 'Unequal'),  
+    ('percent', 'Percentage'),
+    ]
+    expsAmount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
     expsPerson = models.ForeignKey(Person,on_delete=models.CASCADE)
     expense = models.ForeignKey(Expense,on_delete=models.CASCADE)
-    splitType = models.CharField(max_length=50,default='equal')
+    splitType = models.CharField(
+        max_length=50,
+        default='equal',
+        choices=SPLIT_TYPE
+        )
+
+   
+    def clean(self):
+        error = {}
+
+        if self.expsAmount <=0:
+            error['amount'] = f'Expense Value Cannot be Negative or Zero'
+        
+        if self.expsPerson not in self.expense.group.members.all():
+            error['payer'] = f'The {self.payer.name} is not in {self.group.groupName}'
+
+        if error:
+            raise ValidationError(error)
+    
 
     def __str__(self):
         return  self.expsPerson.name + ' shares ' + str(self.expsAmount) +  ' in ' + self.expense.expenseName +"Of " + self.expense.group.groupName
+  
+    def save(self,*args,**kwargs):
+        self.full_clean()
+        return super().save()
 
 class Settlement(models.Model):
     payer = models.ForeignKey(Person,on_delete=models.CASCADE,related_name='amount_payer')
     payee = models.ForeignKey(Person,on_delete=models.CASCADE,related_name='amount_receiver')
     date = models.DateTimeField(auto_now=True)
-    amount = models.FloatField(default=0.0)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
     group = models.ForeignKey(Group,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -58,22 +109,42 @@ class Settlement(models.Model):
     def clean(self):
         if self.payee.id == self.payer.id:
             raise ValidationError('Payer and Payee should not be same')
-        elif (self.amount==0.0):
+        elif self.amount<=0.0:
             raise ValidationError('Amount could not be 0.0')
 
     def save(self,*args,**kwargs):
-        self.clean()
+        self.full_clean()
         return super().save()
+    
     
 class Balance(models.Model):
     person = models.ForeignKey(Person,on_delete=models.CASCADE,related_name="owing_person")
-    amount = models.FloatField(default=0.0)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
     updated_on = models.DateTimeField(auto_now=True)
     group = models.ForeignKey(Group,on_delete=models.CASCADE)
     rPerson  = models.ForeignKey(Person,on_delete=models.CASCADE,related_name="receive_person",default=2)
     def __str__(self):
         return str(self.person) + ' has balance ' + str(self.amount) +' with ' + self.rPerson.name + ' in ' + self.group.groupName
+    
+    def clean(self):
+        if self.person == self.rPerson:
+            raise ValidationError('Person and rPerson should not be same')
+        
+        members = self.group.members.all()
 
+        if self.person not in members :
+            raise ValidationError(f'{self.person} not in the {self.group}')
+        
+        if self.rPerson not in members:
+            raise ValidationError(f'{self.rPerson} not in the {self.group}')
+
+    def save(self,*args,**kwargs):
+        self.full_clean()
+        return super().save()
+   
 
 class SettleTran(models.Model):
     balance = models.ForeignKey(Balance,on_delete=models.CASCADE)
